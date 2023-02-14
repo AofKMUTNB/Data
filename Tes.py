54414a1663cf3dc30def14988fa1f2364f99efee
AofKMUTNB <s6103081610247@email.kmutnb.ac.th>
2023-02-14 16:14:35 +0700

    Make Grade Calcula

--- Tes.py
+++ Tes.py
@@ -1,1 +1,16 @@
-print("Hello")
+print("Welcome to the Grade Calculator")
+
+# Input the score
+score = float(input("Enter your score: "))
+
+# Determine the grade
+if score >= 90:
+    print("Your grade is A")
+elif score >= 80:
+    print("Your grade is B")
+elif score >= 70:
+    print("Your grade is C")
+elif score >= 60:
+    print("Your grade is D")
+else:
+    print("Your grade is F")
