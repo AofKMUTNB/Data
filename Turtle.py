18aaa48bce04872024804609c5cb6c3dd326594f
AofKMUTNB <s6103081610247@email.kmutnb.ac.th>
2023-02-16 14:03:26 +0700

    Edit

--- Turtle.py
+++ Turtle.py
@@ -1,5 +1,25 @@
 import turtle
-t = turtle.Turtle()
-for i in range(10,100,5):
-    t.circle(i)
-    t.forward(5)
+t = turtle.Pen()
+t.shape('turtle')
+for i in range(3):
+    t.forward(50)
+    t.left(90)
+    t.forward(50)
+    t.right(90)
+
+t.penup()
+t.goto(0.0)
+t.right(90)
+t.pendown()
+for i in range(3):
+    t.forward(50)
+    t.right(90)
+    t.forward(50)
+    t.left(90)
+    
+t.right(180)
+for i in range(6):
+    t.forward(50)
+    t.right(90)
+    t.forward(50)
+    t.left(90)
