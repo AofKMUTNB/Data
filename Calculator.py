27b210d8f7a9a2b009b633ef7f3c1922917055c8
AofKMUTNB <s6103081610247@email.kmutnb.ac.th>
2023-02-16 15:03:57 +0700

    Make Calculator

--- Calculator.py
+++ Calculator.py
@@ -9,14 +9,8 @@ print("Sum of the numbers: ", num1 + num2)
 # Subtraction
 print("Difference of the numbers: ", num1 - num2)
 
-# Multiplication
+
 print("Product of the numbers: ", num1 * num2)
 
-# Division
+
 print("Quotient of the numbers: ", num1 / num2)
-
-# Exponentiation
-print("Result of exponentiation: ", num1 ** num2)
-
-# Modulo
-print("Remainder of division: ", num1 % num2)
