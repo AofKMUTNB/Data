863d414a7332a5585a465eb9d8780cb8e428c024
AofKMUTNB <s6103081610247@email.kmutnb.ac.th>
2023-02-16 14:20:59 +0700

    Make Calculator

---
+++ Calculator.py
@@ -0,0 +1,22 @@
+print("Welcome to the Calculator")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Addition
+print("Sum of the numbers: ", num1 + num2)
+
+# Subtraction
+print("Difference of the numbers: ", num1 - num2)
+
+# Multiplication
+print("Product of the numbers: ", num1 * num2)
+
+# Division
+print("Quotient of the numbers: ", num1 / num2)
+
+# Exponentiation
+print("Result of exponentiation: ", num1 ** num2)
+
+# Modulo
+print("Remainder of division: ", num1 % num2)
